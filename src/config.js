39b188163916193ffac19b4bd660c1d0a8c7673a
Angela Masajo <aabmasajo@gmail.com>
2021-01-07 22:42:27 -0600

    main app works

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$xj24K/951TYolpuRUEaxgO3q7reQc1/osirXtcWR7VmVfQyWH7go6',
 }
